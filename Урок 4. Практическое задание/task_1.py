e39afa22df624c3afe5adf83146c42a1a294a553
makroguzov <valera.nedbai.developer@gmail.com>
2022-02-12 19:03:33 +0300

    Домашнее задание №4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,12 +6,12 @@
 
 Сделайте замеры времени выполнения кода с помощью модуля timeit
 
-Попробуйте оптимизировать код, чтобы снизить время выполнения
+Попробуйте оптимизировать код, чтобы снизить время выполнения.
 Проведите повторные замеры
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
-
+import random
 from timeit import timeit
 
 
@@ -21,3 +21,16 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_11(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+if __name__ == '__main__':
+    numbers = [random.randint(1, 100) for _ in range(1000)]
+
+    print(timeit('func_1(numbers)', globals=globals(), number=10000))
+
+    # List comprehension work faster
+    print(timeit('func_11(numbers)', globals=globals(), number=10000))
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,16 +11,18 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
 """
+from random import randint
+from timeit import timeit
 
 
-def revers(enter_num, revers_num=0):
+def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        return revers_1(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -34,4 +36,84 @@ def revers_2(enter_num, revers_num=0):
 def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
-    return revers_num
+    return int(revers_num)
+
+
+def revers_4(enter_num):
+    return int("".join(reversed(str(enter_num))))
+
+
+num_100 = randint(10000, 1000000)
+num_1000 = randint(1000000, 10000000)
+num_10000 = randint(100000000, 10000000000000)
+
+print('\nrevers_1')
+print(
+    timeit(
+        "revers_1(num_100)",
+        setup='from __main__ import revers_1, num_100',
+        number=10000))
+print(
+    timeit(
+        "revers_1(num_1000)",
+        setup='from __main__ import revers_1, num_1000',
+        number=10000))
+print(
+    timeit(
+        "revers_1(num_10000)",
+        setup='from __main__ import revers_1, num_10000',
+        number=10000))
+
+print('\nrevers_2')
+print(
+    timeit(
+        "revers_2(num_100)",
+        setup='from __main__ import revers_2, num_100',
+        number=10000))
+print(
+    timeit(
+        "revers_2(num_1000)",
+        setup='from __main__ import revers_2, num_1000',
+        number=10000))
+print(
+    timeit(
+        "revers_2(num_10000)",
+        setup='from __main__ import revers_2, num_10000',
+        number=10000))
+
+print('\nrevers_3')
+print(
+    timeit(
+        "revers_3(num_100)",
+        setup='from __main__ import revers_3, num_100',
+        number=10000))
+print(
+    timeit(
+        "revers_3(num_1000)",
+        setup='from __main__ import revers_3, num_1000',
+        number=10000))
+print(
+    timeit(
+        "revers_3(num_10000)",
+        setup='from __main__ import revers_3, num_10000',
+        number=10000))
+
+print('\nrevers_4')
+print(
+    timeit(
+        "revers_4(num_100)",
+        setup='from __main__ import revers_4, num_100',
+        number=10000))
+print(
+    timeit(
+        "revers_4(num_1000)",
+        setup='from __main__ import revers_4, num_1000',
+        number=10000))
+print(
+    timeit(
+        "revers_4(num_10000)",
+        setup='from __main__ import revers_4, num_10000',
+        number=10000))
+
+if __name__ == '__main__':
+    pass
